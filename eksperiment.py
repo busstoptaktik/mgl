b437860e1cadc22538691d9d6410b04a859ec79e
Thomas Knudsen <knudsen.thomas@gmail.com>
2020-04-01 08:37:16 +0200

    Initial commit

---
+++ eksperiment.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# observationer = pd.read_excel("KDI2018vest.xlsx", sheet_name="Observationer")
+det_hele = pd.read_excel("KDI2018vest.xlsx", sheet_name=None)
+
+print(80*"-")
+print(det_hele.keys())   # Navnene på alle ark i mappen
+print(80*"-")
+print(det_hele["Beregning"].head())
+print(80*"-")
+print(det_hele["Observationer"].head())
+print(80*"-")
+nogle_punktnavne = det_hele["Observationer"]["Fra"].head()
+print(nogle_punktnavne)
+print(80*"-")
+nogle_højdeforskelle = det_hele["Observationer"]["dH"].head()
+print(nogle_højdeforskelle)
+print(80*"-")
+plt.plot(nogle_højdeforskelle)
